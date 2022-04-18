79c1efd52f8a45b9bb27b106fb6989357b2e197c
miah <xxx>
2022-04-19 00:02:18 +0900

    合計　小計　クラス追加

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 // テーブル形式の位置特定用　（クラスx,クラスy）
-// day1 row1	day2 row1
+// day1 row1 row1_a	day2 row1 row1_a  ⇒　同じ列にrowを２種類作る場合　無印が全体合計　
 // day1 row2	day2 row2
 // day1 row3	day2 row3
 // day1 row4	day2 row4
